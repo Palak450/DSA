1348d63a3de553f51b3d326d75052fe44cdb2b92
Palak450 <palakverma2510@gmail.com>
2022-05-26 13:36:59 +0530

    Learning java

---
+++ src/ConditionsAndLoops/IfStatement.java
@@ -0,0 +1,21 @@
+package ConditionsAndLoops;
+
+import java.util.Scanner;
+
+public class IfStatement {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int marks=sc.nextInt();
+        if(marks>90) {
+
+            System.out.println("A");
+        }
+        else if(marks<80){
+            System.out.println("B");
+        }
+        else{
+            System.out.println("C");
+        }
+
+    }
+}
