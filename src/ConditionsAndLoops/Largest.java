ad5dad0b0981ca3a9868b31ff3d44528c76f7e46
Palak450 <palakverma2510@gmail.com>
2022-06-04 00:22:11 +0530

    Learning java

---
+++ src/ConditionsAndLoops/Largest.java
@@ -0,0 +1,23 @@
+package ConditionsAndLoops;
+
+import java.net.SocketOption;
+import java.util.Scanner;
+
+public class Largest {
+    public static void main(String args[]) {
+        Scanner sc= new Scanner(System.in);
+        int a= sc.nextInt();
+        int b= sc.nextInt();
+        int c= sc.nextInt();
+    if(a>b) {
+        System.out.println(a);
+
+    }
+    else if(c>b){
+        System.out.println(c);
+        }
+else{
+        System.out.println(b);
+    }
+    }
+}
