13a9f748c7232214bbbd1dbc945a886441bf9848
Palak450 <palakverma2510@gmail.com>
2022-06-28 01:02:23 +0530

    Merge remote-tracking branch 'origin/master'

--- src/ConditionsAndLoops/Reverse.java
+++ src/ConditionsAndLoops/Reverse.java
@@ -1,4 +1,17 @@
 package ConditionsAndLoops;
 
 public class Reverse {
+    public static void main(String[] args) {
+
+        int n = 45536;
+        int ans = 0;
+        while (n > 0) {
+            int rem = n % 10;
+            n /= 10;
+            ans = ans * 10 + rem;
+
+        }
+        System.out.println(ans);
+    }
 }
+
